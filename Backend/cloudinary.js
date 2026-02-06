4fd2e52e661acefe0a80e196c812de1b7f843cf1
Sourish73 <sinhasourish.3799@gmail.com>
2026-02-06 21:46:15 +0530

    Move Cloudinary credentials to env variables

--- Backend/cloudinary.js
+++ Backend/cloudinary.js
@@ -1,9 +1,9 @@
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require("cloudinary").v2;
 
-cloudinary.config({ 
-  cloud_name: 'dngqkykzc', 
-  api_key: '538278423866846', 
-  api_secret: 'mfVlKFu9M58K9k_s4qt_kxvJ478'
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
